fadcb26c0857ecaf3d1ac0759b77390c04381206
musiksaibot <tkisum@gmail.com>
2022-03-07 01:55:09 +0100

    added rBergomi start

---
+++ rBergomi/rBergomi.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class rBergomi():
+    def __init__(self, T: float, H: float, eta: float, rho: float):
+        self.T = T
+        self.H = H
+        self.eta = eta
+        self.rho = rho
+
